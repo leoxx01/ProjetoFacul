2b90c23cde915891512e34cace8e4d4b2e251aca
leo <leocarminhoto@gmail.com>
2020-06-06 18:45:08 -0300

    Pagina buy completa

--- app/src/main/java/com/example/aplication/buy.java
+++ app/src/main/java/com/example/aplication/buy.java
@@ -2,7 +2,9 @@ package com.example.aplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class buy extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class buy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
+    }
+    public void sair(View view){
+        finish();
+        System.exit(0);
+    }
+    public void perfilcall(View view){
+        Intent chamar = new Intent(this, perfil.class);
+        startActivity(chamar);
     }
 }
