123b549e24f7a8cc4bf87f632ab1ff557c967aef
leo <leocarminhoto@gmail.com>
2020-06-10 13:26:01 -0300

    Get Dados e PopUp

--- app/src/main/java/com/example/aplication/cadastro.java
+++ app/src/main/java/com/example/aplication/cadastro.java
@@ -4,12 +4,33 @@ package com.example.aplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class cadastro extends AppCompatActivity {
-
+    String nome = null;
+    String email = null;
+    String senha = null;
+    String confirmationsenha = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+    }
+    public void cadastrar(View view){
+        EditText camponome = findViewById(R.id.editText);
+        EditText campoemail = findViewById(R.id.editText2);
+        EditText camposenha = findViewById(R.id.editText5);
+        EditText campoconfirm = findViewById(R.id.editText4);
+
+        nome = camponome.getText().toString();
+        email = campoemail.getText().toString();
+        senha = camposenha.getText().toString();
+        confirmationsenha = campoconfirm.getText().toString();
+
+        Toast.makeText(cadastro.this, "Cadastro Efetuado Com Sucesso", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/aplication/perfil.java
+++ app/src/main/java/com/example/aplication/perfil.java
@@ -5,10 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.AlteredCharSequence;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class perfil extends AppCompatActivity {
 
+    private AlteredCharSequence Snackbar;
+    String camponome = null;
+    String campoemail = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,4 +33,47 @@ public class perfil extends AppCompatActivity {
         Intent chamar = new Intent(this, buy.class);
         startActivity(chamar);
     }
+    public void editar(View view){
+        Button editarbtn = findViewById(R.id.button4);
+        Button salvar = findViewById(R.id.button8);
+
+        EditText nome = findViewById(R.id.editText3);
+        EditText email = findViewById(R.id.editText6);
+
+        TextView labelum = findViewById(R.id.textView4);
+        TextView labeldois = findViewById(R.id.textView3);
+
+        salvar.setVisibility(View.VISIBLE);
+        nome.setVisibility(View.VISIBLE);
+        email.setVisibility(View.VISIBLE);
+
+        editarbtn.setVisibility(View.INVISIBLE);
+        labelum.setVisibility(View.INVISIBLE);
+        labeldois.setVisibility(View.INVISIBLE);
+    }
+    public void salvar(View view){
+        Button editarbtn = findViewById(R.id.button4);
+        Button salvar = findViewById(R.id.button8);
+
+        EditText nome = findViewById(R.id.editText3);
+        EditText email = findViewById(R.id.editText6);
+
+        TextView labelum = findViewById(R.id.textView4);
+        TextView labeldois = findViewById(R.id.textView3);
+
+        camponome  = nome.getText().toString();
+        campoemail = nome.getText().toString();
+
+        salvar.setVisibility(View.INVISIBLE);
+        nome.setVisibility(View.INVISIBLE);
+        email.setVisibility(View.INVISIBLE);
+
+        editarbtn.setVisibility(View.VISIBLE);
+        labelum.setVisibility(View.VISIBLE);
+        labeldois.setVisibility(View.VISIBLE);
+
+
+        Toast.makeText(perfil.this, "Alteração Efetuada Com Sucesso", Toast.LENGTH_LONG).show();
+    }
+
 }
